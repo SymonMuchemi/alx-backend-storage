c17238a39d228a2e029cc1fcdd11e32a12030b83
symonmuchemi <simonkenya846@gmail.com>
2024-12-08 16:43:29 +0300

    task: storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,6 +23,20 @@ def count_calls(method: Callable) -> Callable:
 
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to count how many times a method is called."""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        input_list = method.__qualname__ + ':inputs'
+        output_list = method.__qualname__ + ':outputs'
+
+        self._redis.rpush(input_list, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_list, str(result))
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
 
 class Cache:
     """Redis cache blueprint"""
@@ -33,6 +47,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: REDIS_TYPES) -> str:
         """Stores data in Redis and returns a unique key."""
         key = str(uuid.uuid4())
