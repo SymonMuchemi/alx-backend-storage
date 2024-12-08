096ff9b2adc36afc8a61a959ee79477cc57f0658
symonmuchemi <simonkenya846@gmail.com>
2024-12-08 16:31:08 +0300

    task: incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,11 +3,25 @@
 
 
 import uuid
+from functools import wraps
 from typing import Union, Callable, Any
 import redis
 
 
 REDIS_TYPES = Union[str, bytes, memoryview]
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count how many times a method is called."""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        key = method.__qualname__
+
+        self._redis.incr(key)
+
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -18,11 +32,11 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[REDIS_TYPES]) -> str:
-        """Stores data in Redis and returns the key."""
+    @count_calls
+    def store(self, data: REDIS_TYPES) -> str:
+        """Stores data in Redis and returns a unique key."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
-
         return key
 
     def get(self, key: str, fn: Callable[[REDIS_TYPES], Any] = None) -> Any:
@@ -37,9 +51,7 @@ class Cache:
 
     def get_str(self, key: str) -> Union[str, None]:
         """Retrieves data as a string."""
-        return self.get(
-            key,
-            lambda d: d.decode('utf-8') if isinstance(d, bytes) else d)
+        return self.get(key, lambda d: d.decode("utf-8") if isinstance(d, bytes) else d)
 
     def get_int(self, key: str) -> Union[int, None]:
         """Retrieves data as an integer."""
